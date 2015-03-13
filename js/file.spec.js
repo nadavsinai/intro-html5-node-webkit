58fec04583419a7bfe46a71f6d0ad42e4a8a296c
Nadav Sinai <ns@nadavsinai.com>
2015-03-13 11:34:13 +0200

    third test

--- js/file.spec.js
+++ js/file.spec.js
@@ -1,8 +1,15 @@
 describe('file module tests', function () {
-    var file, fs = require('fs');
+    var file, fs = require('fs'), testArea;
 
     beforeEach(function () {
         file = require('file.js');
+        testArea = document.createElement('textarea');
+        testArea.setAttribute('id', 'editor');
+        document.body.appendChild(testArea);
+    });
+
+    afterEach(function () {
+        document.body.removeChild(testArea);
     });
 
     it('checks that functions are defined', function () {
@@ -11,13 +18,20 @@ describe('file module tests', function () {
     });
 
     it('checks that open reads a file then gets its content into the editor', function () {
-        var testArea = document.createElement('textarea');
-        testArea.setAttribute('id', 'editor');
-        document.body.appendChild(testArea);
         fs.readFile = function () {
-            arguments[arguments.length - 1].call(this, '',123);
+            arguments[arguments.length - 1].call(this, '', 123);
         };
-        file.open('',document);
+        file.open('', document);
         expect(document.getElementById('editor').value).toBe('123');
     });
+
+    it('checks that save calls writeFile with the right arguments', function () {
+        testArea.value = 123;
+        fs.writeFile = function () {
+        };
+        spyOn(fs, 'writeFile');
+        file.save('test', document);
+        expect(fs.writeFile).toHaveBeenCalled();
+        expect(fs.writeFile).toHaveBeenCalledWith('test', '123');
+    })
 });
