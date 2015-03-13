4fdb50fb4921a27b30bc50e9a9c7cea73bf2d39c
Nadav Sinai <ns@nadavsinai.com>
2015-03-13 11:20:07 +0200

    second test

--- js/file.spec.js
+++ js/file.spec.js
@@ -1,12 +1,23 @@
-describe('file module tests',function(){
-    var file,fs=require('fs'),doc = document;
-   beforeEach(function () {
-        file = require('file.js');
-   });
+describe('file module tests', function () {
+    var file, fs = require('fs');
 
-    it('checks that functions are defined',function(){
+    beforeEach(function () {
+        file = require('file.js');
+    });
+
+    it('checks that functions are defined', function () {
         expect(file.open).toBeDefined();
         expect(file.save).toBeDefined();
     });
 
+    it('checks that open reads a file then gets its content into the editor', function () {
+        var testArea = document.createElement('textarea');
+        testArea.setAttribute('id', 'editor');
+        document.body.appendChild(testArea);
+        fs.readFile = function () {
+            arguments[arguments.length - 1].call(this, '',123);
+        };
+        file.open('',document);
+        expect(document.getElementById('editor').value).toBe('123');
+    });
 });
